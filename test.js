5b3701ec5df5d5a1936b5d0729419e2af663c614
butterflycxmillaa <camilla.butterflydev@gmail.com>
2025-08-12 13:52:27 +0200

    implemented coordinate system

--- test.js
+++ test.js
@@ -1,12 +1,20 @@
 class Graph {
     table = [
-        [[1,2],[3,3]],
-        [[0,2,3,4],[3,6,2,3]],
-        [[0,1,4,5],[3,6,4,7]],
-        [[1,4,5],[2,4,5]],
-        [[1,2,3,5],[3,4,4,2]],
-        [[2,3,4],[7,5,2]]
+        [0,0,[1,2]],
+        [4,0,[0,3,4]],
+        [0,5,[0]],
+        [4,3,[1,5,7,9]],
+        [7,0,[1,5]],
+        [7,3,[3,4,6]],
+        [7,8,[5,7]],
+        [4,8,[3,6,8]],
+        [2,8,[7,9]],
+        [2,3,[3,8]]
     ]
+
+    createNode(x,y) {
+        this.table.push([x,y,[]])
+    }
 
     findShortestPath = (s,e) => {
         return this.shortestPath(e,new Array(this.table.length).fill(0),[s])
@@ -19,10 +27,19 @@ class Graph {
         let shortDist = Infinity
         let shortPath = p
         let newMap = [...b.slice(0,s),1,...b.slice(s+1,b.length)]
-        for(let node in this.table[s][0]) {
-            let newPath = [...p,this.table[s][0][node]]
+        for(let node in this.table[s][2]) {
+            let id = this.table[s][2][node]
+            let newPath = [...p,id]
             let params = this.shortestPath(e,newMap,newPath)
-            let dist = this.table[s][1][node] + params[0]
+            let x = this.table[id][0]
+            let y = this.table[id][1]
+            let dist = Math.sqrt(
+                Math.pow(this.table[s][0] - x,2) +
+                Math.pow(this.table[s][1] - y,2)
+            )
+            console.log(s,this.table[s][2][node],this.table[s][0],this.table[s][1],
+                this.table[node][0],this.table[node][1], dist)
+            dist += params[0]
             if(dist < shortDist) {
                 shortDist = dist
                 shortPath = params[1]
@@ -33,4 +50,4 @@ class Graph {
 }
 
 let g = new Graph()
-console.log(g.findShortestPath(5,1))
+console.log(g.findShortestPath(0,7))
